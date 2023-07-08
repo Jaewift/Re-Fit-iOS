ccf6212c43d0b53229462162455b0c37d77e766c
JaeKyu Park <pjk971113@naver.com>
2023-07-08 17:08:55 +1200

    [Feat] 텍스트필드 편집 기능 및 동의사항 팝업창 구현 완료

--- RE-FIT/Login/LoginViewController.swift
+++ RE-FIT/Login/LoginViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var IDTextField: UITextField!
     @IBOutlet weak var PWTextField: UITextField!
@@ -17,7 +17,31 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        IDTextField.layer.cornerRadius = 5
+        IDTextField.layer.borderWidth = 1
+        IDTextField.layer.borderColor = UIColor.clear.cgColor
+        PWTextField.layer.cornerRadius = 5
+        PWTextField.layer.borderWidth = 1
+        PWTextField.layer.borderColor = UIColor.clear.cgColor
+        
+        IDTextField.delegate = self
+        PWTextField.delegate = self
+        textFieldDidBeginEditing(IDTextField)
+        textFieldDidBeginEditing(PWTextField)
+        textFieldDidEndEditing(IDTextField)
+        textFieldDidEndEditing(PWTextField)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.layer.borderColor = UIColor(red: 36/255, green: 117/255, blue: 53/255, alpha: 1).cgColor//your color
+        textField.layer.cornerRadius = 5
+        textField.layer.borderWidth = 1.0
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.layer.borderColor = UIColor.clear.cgColor
+        textField.layer.cornerRadius = 5
+        textField.layer.borderWidth = 1.0
     }
     
     @IBAction func ShowPassword(_ sender: Any) {
@@ -29,7 +53,7 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func JoinButton(_ sender: Any) {
-        guard let rvc = self.storyboard?.instantiateViewController(withIdentifier: "JoinViewController") as? JoinViewController else {return}
+        guard let rvc = self.storyboard?.instantiateViewController(withIdentifier: "JoinVC") as? JoinViewController else {return}
         
         self.navigationController?.pushViewController(rvc, animated: true)
     }
